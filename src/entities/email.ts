6d55a6ec3c3f511ee7d75ffcca2e15afa59f172b
Alexei <alexeiOrlov.dev@gmail.com>
2021-04-23 22:37:33 +0300

    fix(entities): custom error

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,24 +1,16 @@
-import * as t from "runtypes"
+import * as t from "runtypes";
 
-function validate(input: string):boolean{
+function validate(input: string): boolean {
   const inputElement = document.createElement("input");
   inputElement.type = "email";
-  inputElement.value = input
-  const valid = inputElement.validity.valid
-  inputElement.remove()
-  return valid
+  inputElement.value = input;
+  const valid = inputElement.validity.valid;
+  inputElement.remove();
+  return valid;
 }
 
-export const Email = t.String.withBrand("Email").withConstraint(input => {
+export const Email = t.String.withBrand("Email").withConstraint(
+  (input) => validate(input) || `Input: ${input} cannot be converted to email`
+);
 
-  const valid = validate(input)
-
-  if(!valid){
-    throw new TypeError(`Input: ${input} cannot be converted to email`);
-  }
-
-  return input
-})
-
-
-export type Email = t.Static<typeof Email>
+export type Email = t.Static<typeof Email>;
--- src/entities/password.ts
+++ src/entities/password.ts
@@ -1,21 +1,14 @@
-import * as t from "runtypes"
+import * as t from "runtypes";
 
 const VALIDATOR = /^[a-zA-Z1-9._]+@*\.com$/;
 
-function validate(input: string):boolean{
-  return VALIDATOR.test(input)
+function validate(input: string): boolean {
+  return VALIDATOR.test(input);
 }
 
-export const Password = t.String.withBrand("Password").withConstraint(input => {
+export const Password = t.String.withBrand("Password").withConstraint(
+  (input) =>
+    validate(input) || `Input: ${input} cannot be converted to password`
+);
 
-  const valid = validate(input)
-
-  if(!valid){
-    throw new TypeError(`Input: ${input} cannot be converted to email`);
-  }
-
-  return input
-})
-
-
-export type Password = t.Static<typeof Password>
+export type Password = t.Static<typeof Password>;
